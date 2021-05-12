2e75696df16f65b94b49f75834d40d5cdba0974f
Matthew Lonis <mattlonis@gmail.com>
2021-05-12 10:45:22 -0400

    Data Binding in Angular Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,6 +12,7 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
     UserSettingsFormComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule
   ],
